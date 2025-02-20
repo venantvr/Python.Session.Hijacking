3fb1d6509522299b76a81b12af1518375ae0cfae
venantvr <r.venant.valery@gmail.com>
2025-02-20 14:37:50 +0100

    First commit...

--- static/hijack.js
+++ static/hijack.js
@@ -1,9 +1,9 @@
 function hijackSession() {
-    const sessionId = document.getElementById('session_id').value.trim();
+    const sessionId = document.getElementById("session_id").value.trim();
     if (!sessionId) {
-        document.getElementById('error').innerText = 'Veuillez entrer un Session ID';
+        document.getElementById("error").innerText = "Veuillez entrer un Session ID";
         return;
     }
     document.cookie = `session=${encodeURIComponent(sessionId)}; path=/`;
-    window.location.href = '/secret';
+    window.location.href = "/secret";
 }
